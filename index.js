2bb5653d5ea5a6f33362495a6f326d9a26e353c2
unknown <xianwei9527@gmail.com>
2020-01-26 14:44:42 -0500

    organized reuseable code into the autocomplete function, in order to be used when creating second search

--- index.js
+++ index.js
@@ -1,90 +1,39 @@
-// created async/await functions to fech data by using axios
-const fetchData = async searchTerm => {
-    const res = await axios.get("http://www.omdbapi.com/", {
-        // by creating this object it will auto appened these values to the url
-        params: {
-            apikey: "bff1ca7e",
-            // for search purpose
-            s: searchTerm,
-        },
-    });
-    // dealing with api Errors -> that's from the api
-    if (res.data.Error) {
-        // return an empty array
-        return [];
-    }
-    // return data
-    return res.data.Search;
-};
-
-// create a HTML setup for inserting to HTML file
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-<label class="label"><b>Search Moive: </b></label>
-<div class="dropdown">
-    <input class="input is-primary searchBar"/>
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-</div>
-`;
-
-// selected element from the body
-const searchBar = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-// search bar
-const onSearchBar = async event => {
-    // check see if there's value in the input, if not do no send request
-    if (!event.target.value) return;
-    // await for data to be resolved than stored into movies variable
-    const movies = await fetchData(event.target.value);
-    // check see if there's anythin in the movies
-    if (!movies.length) {
-        // if not remove the class "is-active"
-        dropdown.classList.remove("is-active");
-        return;
-    }
-
-    resultsWrapper.innerHTML = "";
-    // if there's fetch successed add class is-active
-    dropdown.classList.add("is-active");
-    // loop through the data
-    for (let movie of movies) {
-        // create element for each pieces data
-        const option = document.createElement("a");
-        // preventing broken image for no src defined
+createAutoComplete({
+    root: document.querySelector(".autocomplete"),
+    // rendering the data from the api and do some validation with it.
+    renderOption(movie) {
+        // check and see if the poster is url exist or not
         const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-        option.classList.add("dropdown-item");
-        // inserting data to the element created above
-        option.innerHTML = `
+        // return the src and title, and year of the movie
+        return `
             <img src="${imgSrc}" />
-            ${movie.Title}
+            ${movie.Title} (${movie.Year})
         `;
-        // added event listener to each movie option
-        // when selected the search bar value set to movie actual title
-        option.addEventListener("click", () => {
-            dropdown.classList.remove("is-active");
-            searchBar.value = movie.Title;
-            // pass movie object to onMovieSelect fetch function
-            onMovieSelect(movie);
+    },
+    onOptionSelect(movie) {
+        // create another fetch call and passed the current selected movie
+        onMovieSelect(movie);
+    },
+    // return the movie title for own use
+    inputValue(movie) {
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const res = await axios.get("http://www.omdbapi.com/", {
+            // by creating this object it will auto appened these values to the url
+            params: {
+                apikey: "bff1ca7e",
+                s: searchTerm,
+            },
         });
-        // insert it into the result element
-        resultsWrapper.appendChild(option);
-    }
-};
-//                                  preventing send request every single time.
-searchBar.addEventListener("input", delayFetch(onSearchBar, 1000));
-
-// added an event listener to the whole body, except everything within the root tag
-
-document.addEventListener("click", event => {
-    // if user clicked areas that is within root tag
-    if (!root.contains(event.target)) {
-        // remove class is-active
-        dropdown.classList.remove("is-active");
-    }
+        // dealing with api Errors -> that's within the api
+        if (res.data.Error) {
+            // return an empty array
+            return [];
+        }
+        // return data
+        return res.data.Search;
+    },
 });
 
 // fetch data from the that particular movie with detailed info
